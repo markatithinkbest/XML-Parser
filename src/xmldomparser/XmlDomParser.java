157794f90240a756482d6127257a3fa1b5e03f30
Mark Chen <mark@ithinkbest.com>
2015-04-17 13:07:50 +0800

    commit 001

---
+++ src/xmldomparser/XmlDomParser.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xmldomparser;
+
+import javax.xml.parsers.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ *
+ * @author MARK
+ */
+public class XmlDomParser {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse("C:\\xmlfiles\\F-C0032-005.xml");
+            NodeList locationList = doc.getElementsByTagName("location");
+            for (int i = 0; i < locationList.getLength(); i++) {
+                Node p = locationList.item(i);
+                if (p.getNodeType() == Node.ELEMENT_NODE) {
+                    Element location = (Element) p;
+                    NodeList dataList = location.getChildNodes();
+                    for (int j = 0; j < dataList.getLength(); j++) {
+                        Node n = dataList.item(j);
+                        if (n.getNodeType() == Node.ELEMENT_NODE) {
+                            Element data = (Element) n;
+                            if (data.getTagName() == "weatherElement") {
+//                                System.out.println("... going to parse weatherElement");
+
+                                NodeList weatherElementList = data.getChildNodes();
+//                                System.out.println("... going to parse weatherElement, weatherElementList.getLength()=" + weatherElementList.getLength());
+//                                System.out.println("... going to parse weatherElement, item 0 NodeName=" + weatherElementList.item(0).getNodeName());
+
+                                for (int k = 0; k < weatherElementList.getLength(); k++) {
+//                                    System.out.println("......k=" + k);
+                                    Node nodeK = weatherElementList.item(k);
+                                    if (nodeK.getNodeType() == Node.ELEMENT_NODE) {
+                                        Element weather = (Element) nodeK;
+                                        if (weather.getTagName() == "elementName") {
+                                            System.out.println("......" + weather.getTagName() + " " + weather.getTextContent());
+
+                                        } else if (weather.getTagName() == "time") {
+                                            System.out.println("......" + weather.getTagName());
+
+                                            NodeList timeList = weather.getChildNodes();
+
+                                            for (int m = 0; m < timeList.getLength(); m++) {
+//                                    System.out.println("......k=" + k);
+                                                Node nodeM = timeList.item(m);
+                                                if (nodeM.getNodeType() == Node.ELEMENT_NODE) {
+
+                                                    Element time = (Element) nodeM;
+                                                    if (time.getTagName() == "startTime") {
+                                                        System.out.println("........." + time.getTagName() + " " + time.getTextContent());
+
+                                                    } else if (time.getTagName() == "endTime") {
+                                                        System.out.println("........." + time.getTagName() + " " + time.getTextContent());
+
+                                                    } else if (time.getTagName() == "parameter") {
+                                                        System.out.println(".........parameter");
+
+                                                        NodeList parameterList = time.getChildNodes();
+                                                        for (int m2 = 0; m2 < parameterList.getLength(); m2++) {
+//                                    System.out.println("......k=" + k);
+                                                            Node nodeN = parameterList.item(m2);
+                                                            if (nodeN.getNodeType() == Node.ELEMENT_NODE) {
+                                                                Element parameter = (Element) nodeN;
+                                                                System.out.println("............" + parameter.getTagName() + " " + parameter.getTextContent());
+                                                            }else{
+                                                               //    System.out.println("??? WHAT NOT ELEMENT_NODE");
+                                                            }
+                                                        }
+
+                                                    } else {
+                                                        System.out.println("??? NOT CONSIDERED" + time.getTagName() + " " + time.getTextContent());
+                                                    }
+                                                }
+
+                                            }
+
+                                        } else {
+                                            System.out.println("...... WHAT ELSE?");
+
+                                        }
+                                    }
+                                }
+
+//                                for (int k = 0; j < weatherElementList.getLength(); j++) {
+//                                    Node nodeK = weatherElementList.item(k);
+//                                  //  if (nodeK.getNodeType() == Node.ELEMENT_NODE) {
+//                                        Element weather = (Element) nodeK;
+//                                        System.out.println("..." + weather.getTagName() + " " + weather.getTextContent());
+//                                   // }
+//
+//                                }
+                            } else {
+                                System.out.println("..." + data.getTagName() + " " + data.getTextContent());
+                            }
+                        }
+                    }
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
